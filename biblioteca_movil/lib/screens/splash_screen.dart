76b7c118df9f3f120a4a4c4b2b1af36b73ba39c5
LuisAr241 <sarluis2425@gmail.com>
2026-04-02 19:53:49 -0400

    Feat: improving splash screen design

--- biblioteca_movil/lib/screens/splash_screen.dart
+++ biblioteca_movil/lib/screens/splash_screen.dart
@@ -7,23 +7,73 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+
   @override
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 2), () {
-      if (!mounted) return;
+  
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
 
+    _opacity = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward();
+
+    // Navegación
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
       Navigator.pushReplacementNamed(context, '/home');
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
       body: Center(
-        child: Text("Biblioteca App 📚", style: TextStyle(fontSize: 24)),
+        child: FadeTransition(
+          opacity: _opacity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.menu_book,
+                size: 100,
+                color: Colors.white,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Biblioteca App",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Tu biblioteca personal",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
